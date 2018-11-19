b3e180abb6d96b5cead3f2e156e61a8212a5c8e9
Giankarlo <giankarlocash@gmail.com>
2018-11-19 10:21:56 -0300

    item 4 mas com falha ainda

--- dataguard.c
+++ dataguard.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 
-/*typedef struct {
+/*typedef struct menudsl{
 	printf("\n\t=======================================\n");
     printf("\t=          DataGuard Stn Lee          =\n");
     printf("\t=======================================\n");
@@ -20,16 +20,15 @@
 
 int main(void){
 
-    FILE *fileSl, *fileSt;
+    FILE *fileSl, *fileTabelas;
     int opcao, qtdCol;
     int i, cont;
-    char nomeTabela[50], **tamTabela;
+    char nomeTabela[50], **tamTabela, buscatabela;
 
-   
-    //menu de opções
-	printf("\n\t=======================================\n");
+    printf("\n\t=======================================\n");
     printf("\t=          DataGuard Stn Lee          =\n");
     printf("\t=======================================\n");
+    //menu de opções
     printf("\n\t             Menu de Opções\n\n");
     printf("\t1 - Criar tabela\n");
     printf("\t2 - Listar todas as tabelas\n");
@@ -38,73 +37,92 @@ int main(void){
     printf("\t5 - Pesquisar valor em uma tabela\n");
     printf("\t6 - Apagar valor de uma tabela\n");
     printf("\t7 - Apagar uma tabela\n\n");
-    //printf(menudsl);
 
     printf("Escolha uma opção: ");
     //implementar função "getch()" para ocultar o número da opção digitada.
     scanf("%i", &opcao);
     do{
-        if(opcao == 1){	
-			char listatabela[] = "listab.txt";
-			//fileSl = fopen(nometabela, "w");
-			fileSl = fopen(listatabela, "w");
+        if(opcao == 1){
             //Coleta parâmetros para proseguir com a criação da tabela.
             printf("Informe o nome da tabela a ser criada: ");
-			scanf("%s", nomeTabela);
-            fprintf(fileSl, "%s\n", nomeTabela);
-			fileSt = fopen(nomeTabela, "w");
-            printf("Informe a quantidade de colunas da tabela: ");
-            scanf("%i", &qtdCol);
-            tamTabela = (char**)malloc(sizeof(char*)*qtdCol);
-            for(i = 0; i < qtdCol; i++)
-                tamTabela[i] = (char*)malloc(sizeof(char)*50);
-            //Define os campos da tabela.
-            printf("A 1ª coluna armazerá a chave primária.\n");
-            cont = 1;
-            for(i = 0; i < qtdCol; i++){
-                if(i == qtdCol-1){
-                    printf("Informe o nome da %iª coluna: ", cont);
-                    scanf("%s", tamTabela[i]);
-                    //fprintf(fileSl, "%s\n", tamTabela[i]);
-					fprintf(fileSt, "%s\n", tamTabela[i]);
-                }
+            scanf("%s", nomeTabela);
+             fileSl = fopen(nomeTabela, "w");
+             //arquivo para armazenar apenas o nome das tabelas,
+             //para poder listar os nomes quando solicitado na opção 2.
+             fileTabelas = fopen("nomeTabela.dsl", "a");
+             fprintf(fileTabelas, "%s\n", nomeTabela);
+             printf("Informe a quantidade de colunas da tabela: ");
+             scanf("%i", &qtdCol);
+             tamTabela = (char**)malloc(sizeof(char*)*qtdCol);
+             for(i = 0; i < qtdCol; i++)
+                 tamTabela[i] = (char*)malloc(sizeof(char)*50);
+             //Define os campos da tabela.
+             printf("A 1ª coluna armazerá a chave primária.\n");
+             cont = 1;
+             for(i = 0; i < qtdCol; i++){
+                 if(i == qtdCol-1){
+                     printf("Informe o nome da %iª coluna: ", cont);
+                     scanf("%s", tamTabela[i]);
+                     fprintf(fileSl, "%s\n", tamTabela[i]);
+                 }
                 else{
                     printf("Informe o nome da %iª coluna: ", cont);
                     scanf("%s", tamTabela[i]);
-                    //fprintf(fileSl, "%s ", tamTabela[i]);
-					fprintf(fileSt, "%s ", tamTabela[i]);
+                    fprintf(fileSl, "%s ", tamTabela[i]);
+                    fprintf(fileSl, "%s\n", tamTabela[i]);
                 }
                 cont++;
             }
-            fclose(fileSl);
-			fclose(fileSt);
-            printf("Tabela criada com sucesso.\n");
-        //continue;
+             fclose(fileSl);
+             fclose(fileTabelas);
+             printf("Tabela criada com sucesso.\n");
+         }
+         //Listar todas as tabelas.
+         else if(opcao == 2){
+             fileTabelas = fopen("nomeTabela.dsl", "r");
+             if(fileSl == NULL)
+                 printf("Não foi possivel abrir o arquivo.\n");
+             while(fgets(nomeTabela, 50, fileTabelas) != NULL)
+                 printf("%s", nomeTabela);
+            fclose(fileTabelas);
         }
-        //Listar todas as tabelas.
-        else if(opcao == 2){
-            fileSl = fopen(nomeTabela, "r");
-            if(fileSl == NULL)
-                printf("Não foi possivel abrir o arquivo\n");
-            else{
-                //fscanf(fileSl, "%s\n%s %s %s", nomeTabela, tamTabela[0], tamTabela[1], tamTabela[2]);
-                //printf("%s\n%s %s %s\n", nomeTabela, tamTabela[0], tamTabela[1], tamTabela[2]);
-            }
-            fclose(fileSl);
-        }
+        //Criar uma nova linha na tabela. 
         /*
+		char = buscatabela;
         else if(opcao == 3){
-            Criar uma nova linha na tabela\n
+            printf("Informe o nome da tabela: ");
+            scanf("%s", buscaTabela);
+            fileSl = fopen(buscaTabela, "r");
+            while(fgets(checkBusca, 50, fileSl) != NULL){
+                printf("Informe o valor da coluna %s :", checkBusca);
+            while(fgets(checkBusca, 50, fileSl) != NULL){      
+                printf("Informe o valor da coluna %s", checkBusca);
+                scanf("%s\n", nomeTabela);
+                fileSl = fopen(buscaTabela, "a");
+                fprintf(fileSl, "%s ", buscaTabela);
+                fprintf(fileSl, "%s\n", nomeTabela);
+            fclose(fileSl);
+            }
         }
-        else
-            printf("Opção invalida\n");
         */
+		//listar todos os dados de uma tabela.
+		else if(opcao == 4){
+			printf("Informe o nome da tabela a ser listada: ");
+			scanf("%s", buscaTabela);
+			fileTabelas = fopen("", "r");
+			if(fileSl == NULL)
+                 printf("Não foi possivel abrir o arquivo.\n");
+			while(fgets(checkBusca, 50, fileTabelas) != NULL)
+
+					printf("%s", fileTabelas);
+            fclose(fileTabelas);
+
         printf("Escolha uma opção: ");
         scanf("%i", &opcao);
     }while(opcao != 0);
-    if(opcao == 0)
-        printf("Finalizando programa...\n");
-
-    return 0;
+     if(opcao == 0)
+         printf("Finalizando programa...\n");
+ 
+     return 0;
 }
 //excelsior
